3b057f20aaaf9e849280db236f8ee02f47dc2026
harikrishna76 <harikrishna.salver@gmail.com>
2021-09-04 22:30:43 +0530

    1 file modified

--- src/pages/example1/index.jsx
+++ src/pages/example1/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Example1 = () => {
-  return <div>Showing example 1</div>;
+  return <div>Showing example one</div>;
 };
 
 export default Example1;
